f506718db1667ead3fb67796921220a2e54db1cb
fabien <fabien@civilmaps.com>
2017-05-25 00:51:26 -0700

    making the python script executable

--- scripts/webinar3/get_data.sh
+++ scripts/webinar3/get_data.sh
@@ -2,3 +2,4 @@
 wget https://s3-us-west-1.amazonaws.com/civilmaps-artifacts/webinar3/signal_outlines.json
 wget https://s3-us-west-1.amazonaws.com/civilmaps-artifacts/webinar3/frames.zip
 unzip frames.zip
+mv frames_1 frames
